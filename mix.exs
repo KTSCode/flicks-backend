452739d447694127cacfbc56b63b26b784f3e95b
Kyle San Clemente <ksanclemente@acorns.com>
2020-08-28 12:40:26 -0700

    Fix more conflicts

--- mix.exs
+++ mix.exs
@@ -3,6 +3,18 @@ defmodule Flicks.MixProject do
 
   def project do
     [
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        ignore_warnings: ".dialyzer_ignore.exs",
+        list_unused_filters: true,
+        plt_file: {:no_warn, "flicks.plt"}
+      ],
       app: :flicks,
       version: "0.1.0",
       elixir: "~> 1.7",
@@ -33,15 +45,27 @@ defmodule Flicks.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:vapor, "~> 0.10.0"},
+      {:ex_machina, "~> 2.4.0"},
+      {:cors_plug, "~> 2.0.2"},
+      {:absinthe_phoenix, "~> 2.0.0"},
+      {:absinthe_plug, "~> 1.5.0"},
+      {:absinthe, "~> 1.5.2"},
+      {:git_hooks, "~> 0.5.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.13.1", only: :test},
+      {:credo, "~> 1.4.0", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0.2", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.22.2", only: :dev, runtime: false},
+      {:phoenix, "~> 1.5.4"},
+      {:phoenix_ecto, "~> 4.1.0"},
+      {:ecto_sql, "~> 3.4.5"},
+      {:postgrex, "~> 1.0.0-rc.1"},
+      {:phoenix_live_dashboard, "~> 0.2.7"},
+      {:telemetry_metrics, "~> 0.5.0"},
+      {:telemetry_poller, "~> 0.5.1"},
+      {:jason, "~> 1.2.1"},
+      {:plug_cowboy, "~> 2.3.0"}
     ]
   end
 
